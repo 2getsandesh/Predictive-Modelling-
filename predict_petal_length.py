7b4ed7adb79ca22dc3a2df78dd1ae15ab789b2cd
Sandesh R <2getsandesh@gmail.com>
2024-04-20 17:07:13 +0530

    Update predict_petal_length.py

--- predict_petal_length.py
+++ predict_petal_length.py
@@ -8,11 +8,15 @@ iris = load_iris()
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 df['species'] = iris.target
 
-x=df[['sepal length (cm)']]
+x=df[['sepal length (cm)', 'species']]
 y=df['petal length (cm)']
 
 model=DecisionTreeRegressor(random_state=42)
 model.fit(x,y)
-x1=[[2.5]]
-pred=model.predict(x1)
-print(pred)
+
+x1=[[5.1, 2]]
+pred=model.predict(x)
+
+for actual,predicted in zip(y,pred):
+    print(f"Actual: {actual} predicted: {predicted}")
+
